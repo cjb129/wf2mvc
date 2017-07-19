08a9e52a1f9bce6df6909be9766641d47df2b62c
cjb129 <cjb129@zips.uakron.edu>
2017-07-18 22:25:01 -0400

    Monodevelop has terrible intellisense.

---
+++ PartyInvite/PartyMVC/Models/RSVP.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PartyMVC
+{
+	public class RSVP
+	{
+		public string name {
+			get;
+			set;
+		}
+
+		public string rsvp {
+			get;
+			set;
+		}
+		public RSVP ()
+		{
+		}
+	}
+}
+
